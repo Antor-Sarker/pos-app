2962d31e599e49a397de4c5db5da4deaa381333a
antor sarker <antorsarker55@gmail.com>
2024-02-25 16:30:10 +0600

    handel search and category filter

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import Filter from "./filter/filter";
 
 function Product({ data,handelAddToCart }) {
@@ -14,12 +15,37 @@ function Product({ data,handelAddToCart }) {
     </>
   );
 }
+
 export default function Products({ productsData, handelAddToCart }) {
+  const [isFilter, setIsFilter] = useState(false)
+  const [filterData, setFilterData] = useState([])
+
+  const data = isFilter? filterData : productsData
+
+  console.log(data)
+
+  function handelSearch(key){
+    setIsFilter(true)
+    const foundData = productsData.filter(item=> item.name.toLocaleLowerCase().includes(key.toLocaleLowerCase()))
+    setFilterData(foundData)
+  }
+
+  function handelCategory(category){
+  if(category==='all'){
+    setIsFilter(false)
+    return
+  }
+    setIsFilter(true)
+    const foundData = productsData.filter(item=> item.category===category)
+    setFilterData(foundData)
+  }
+
+  
   return (
     <div className="">
-      <Filter />
+      <Filter setIsFilter={setIsFilter} handelSearch={handelSearch} handelCategory={handelCategory}/>
       <div className="h-4/6 grid grid-cols-5 gap-4 bg-[#f4f6f8] mx-3 overflow-scroll">
-        {productsData.map((data) => (
+        {data.map((data) => (
           <Product key={data.id} data={data} handelAddToCart={handelAddToCart}/>
         ))}
       </div>
--- src/components/products/filter/filter.jsx
+++ src/components/products/filter/filter.jsx
@@ -1,23 +1,24 @@
+/* eslint-disable react/prop-types */
 import { EllipsisVerticalIcon, MagnifyingGlassIcon, QrCodeIcon } from "@heroicons/react/24/outline";
 
-export default function Filter(){
+export default function Filter({handelSearch,handelCategory}){
     return(
         <>
 
         <div className="m-3 flex justify-between bg-[#f9fafb]">
             <div className="flex justify-between">
                 <MagnifyingGlassIcon className="w-6 h-6"/>
-                <input type="search" name="search" id="search" placeholder="Search Products..."/>
+                <input type="search" name="search" id="search" placeholder="Search Products..." onChange={(e)=>handelSearch(e.target.value)}/>
             </div>
             <QrCodeIcon className="w-6 h-6"/>
         </div>
 
         <div className="m-3 flex justify-between text-[#b9c0c6]">
-            <div className="border-2 rounded border-[#b2bac1]">All Categories</div>
-            <div className="border-2 rounded border-[#b2bac1]">Electronics</div>
-            <div className="border-2 rounded border-[#b2bac1]">Home & Lifestyle</div>
-            <div className="border-2 rounded border-[#b2bac1]">Man Fashion</div>
-            <div className="border-2 rounded border-[#b2bac1]">Women Fashions</div>
+            <div className="border-2 rounded border-[#b2bac1]" onClick={()=>handelCategory('all')}>All Categories</div>
+            <div className="border-2 rounded border-[#b2bac1]" onClick={()=>handelCategory('electronics')}>Electronics</div>
+            <div className="border-2 rounded border-[#b2bac1]" onClick={()=>handelCategory('home&lifestyle')}>Home & Lifestyle</div>
+            <div className="border-2 rounded border-[#b2bac1]" onClick={()=>handelCategory('men fashion')}>Man Fashion</div>
+            <div className="border-2 rounded border-[#b2bac1]" onClick={()=>handelCategory('women fashion')}>Women Fashions</div>
             <EllipsisVerticalIcon className="w-6 h-6 text-[#5d6164]"/>
         </div>
 
